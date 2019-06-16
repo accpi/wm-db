dfca5958f4b39e7c25684d6111886bca329985ba
Nelson <nelson.kim.1994@gmail.com>
2019-06-16 18:46:03 -0400

    Deployment Keys

--- westmarches/settings.py
+++ westmarches/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'o!z6k!ckvm*&)9uk9w+fnu$=y6y=w-jt@&4$5a^w_)s+&!9g&b'
-SECRET_KEY = '6390f508b17e72a78a3554e6139c3e8541d3d2aef4f93978'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
